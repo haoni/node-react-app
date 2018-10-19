642a3df3a492396d85525e9a827baff97f4c49c6
haoni <haoni@outlook.com>
2018-10-19 14:33:57 -0400

    A API está pronta para ser usado, basta rodar usando: npm run dev

---
+++ src/config/cors.js
@@ -0,0 +1,8 @@
+module.sxports = function(req, response, next){
+    response.header('Acess-Control-Allow-Origin', '*')
+    response.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    response.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
+    next()
+
+}
