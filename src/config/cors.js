b9575fe554663f9246ac5f4de3e18e7ffd65b8e2
haoni <haoni@outlook.com>
2018-10-29 22:35:13 -0400

    Adicionando comentários no loader de como iniciar o servidor.

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,9 @@
-module.sxports = function(req, response, next){
+module.exports = function(req, response, next){
     response.header('Acess-Control-Allow-Origin', '*')
     response.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     response.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
+    console.log(req)
 
     next()
 
--- src/loader.js
+++ src/loader.js
@@ -2,3 +2,9 @@ const server = require('./config/server')
 
 require('./config/database')
 require('./config/routes')(server)
+
+/*
+    Para iniciar o servido deve-se executar os seguinte comando no terminal do VSCode:
+    -> sudo mongod
+    -> npm run dev
+*/
